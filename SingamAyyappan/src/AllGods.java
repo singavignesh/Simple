8b2fca3d1b660e7d5090cd1ee8a218cd098106a9
DELL <DELL@DESKTOP-5D3KP70>
2021-02-24 10:12:34 +0530

    committ

---
+++ SingamAyyappan/src/AllGods.java
@@ -0,0 +1,16 @@
+
+public class AllGods {
+	
+	public void method1() {
+		System.out.println("all god");
+	}
+
+	public static void main(String[] args) {
+		AllGods ag = new AllGods();
+		ag.method1();
+		
+	
+
+	}
+
+}
